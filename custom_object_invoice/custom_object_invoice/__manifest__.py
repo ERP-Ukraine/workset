a95679ebbf3832d898b73398da1d52bf8ae22752
Karoline.Schmidt <karoline.schmidt@nevaris.com>
2025-08-13 03:11:38 +0200

    custom_object_invoice stub

---
+++ custom_object_invoice/custom_object_invoice/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "custom_object_invoice (stub for uninstall)",
+    "version": "18.0.0.0.0",
+    "summary": "Stub to let the database start and uninstall the old v16 module.",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "license": "LGPL-3"
+}
