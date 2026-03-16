f9c27409b57bd4571ad84d300b5bcfd2b07b725d
BJungWorkSet <b.jung@work-set.eu>
2026-03-16 15:07:30 +0100

    Update __manifest__.py

--- custom_object_invoice/custom_object_invoice/__manifest__.py
+++ custom_object_invoice/custom_object_invoice/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "custom_object_invoice (stub for uninstall)",
-    "version": "19.0.0.0.0",
+    "version": "19.0",
     "summary": "Stub to let the database start and uninstall the old v16 module.",
     "depends": [],
     "data": [],
