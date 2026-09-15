49c0859a451b1a1a5800b82b945d177b358a9ea4
BJungWorkSet <b.jung@work-set.eu>
2026-09-15 21:01:38 +0300

    custom_object_invoice: stub module at top level (installable on 19, to uninstall after upgrade)

---
+++ custom_object_invoice/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "custom_object_invoice (stub for uninstall)",
+    "version": "19.0",
+    "summary": "Stub to let the database start and uninstall the old v16 module.",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "license": "LGPL-3"
+}
